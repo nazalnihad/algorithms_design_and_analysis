669e78041764889a6215dbde004d2a2c402b045b
Nazal-nihad <nazalnihad17@gmail.com>
2023-09-20 15:10:35 +0530

    linear search added

--- linear_search.cpp
+++ linear_search.cpp
@@ -42,3 +42,26 @@ int main()
     }
     return 0;
 }
+/*
+ Algorithm
+  Basic Linear Search
+
+ Input: A list of elements (array) arr.
+         The element to be searched for, key.
+ Output:The index of the key element if found, or -1 if the element is not in the list.
+       Methodology:
+
+1 - Initialize a variable i to 0.
+
+2 - Start a loop that will iterate through each element in the array arr.
+
+3 - Check if arr[i] is equal to the key element.
+
+4 - If they are equal, return the value of i, as you have found the key element.
+
+5 - If they are not equal, increment i by 1 to move to the next element in the array.
+
+6 - Repeat steps 3-5 until you find the key element or reach the end of the array.
+
+7 - If you reach the end of the array without finding the key element, return -1 to indicate that the element is not in the list
+*/
